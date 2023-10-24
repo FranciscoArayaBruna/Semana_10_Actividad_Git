2abaa9584801b2b2a6b3aa490916c10ac41b24a5
FranciscoArayaBruna <143454124+FranciscoArayaBruna@users.noreply.github.com>
2023-10-24 15:55:31 -0300

    "deleted soutPrint"

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -11,10 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Hola mundo");
-        System.out.println("Hola mundo");
-        System.out.println("Hola mundo");
-        System.out.println("Hola mundo");
+
 
     }
 }
